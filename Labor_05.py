691439b5a9608d05293e01ac8a780c60061d5470
BKornel5 <bertokkori05@gmail.com>
2024-11-04 19:50:13 +0100

    Ötödik módosítás

--- Labor_05.py
+++ Labor_05.py
@@ -1,23 +1,3 @@
-from math import trunc
-
 from Labor_05_modul import *
 
-def beleptetes():
-    pass
-
-
-def regisztracio():
-    pass
-
-
-def jelszo_ellenorzes():
-    pass
-
-
-# főprogram
-#regisztracio()
-#beleptetes()
-felhasznalo_neve = felhasznalonev()
-print(felhasznalo_neve)
-felhasznalo_jelszava = jelszo_bekerese(8)
-print(felhasznalo_jelszava)
+beleptetes()
